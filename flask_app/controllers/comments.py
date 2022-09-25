f1d27cf973a3a3f6588c103339f4c7524889ac9b
Maverick Sison <mavericksison@outlook.com>
2022-09-25 10:48:04 -0700

    completed controllers for sightings and comments

--- flask_app/controllers/comments.py
+++ flask_app/controllers/comments.py
@@ -1,3 +1,43 @@
 from flask_app import app
 from flask import render_template, redirect, request, session, flash
 from flask_app.models.comment import Comment
+
+
+#add_comment_form - will sit in the sighting view page, dont need a comment page
+
+#add_comment_db
+@app.route("/comment/create/<int:sighting_id>", methods=["POST"])
+def create_comment(sighting_id):
+    #1 Validate, request form its a function fask 
+    if not Comment.validate_comment(request.form):
+        #2 Always  redirect to the reoute that you were at last!
+        return redirect(f"/sighting/{sighting_id}")
+    data = {
+        "content": request.form["content"]
+    }
+    Comment.add_comment(data)
+    return redirect(f"/sighting/{sighting_id}")
+
+
+#sighting_id redirects back; comment_id knows which one update etc
+@app.route("/comment/update/<int:comment_id>/<int:sighting_id>", methods=["POST"])
+def update_comment(comment_id, sighting_id):
+    #1 Validate, request form its a function fask 
+    if not Comment.validate_comment(request.form):
+        #2 Always  redirect to the reoute that you were at last!
+        return redirect(f"/sighting/{sighting_id}")
+    data = {
+        "id": comment_id,
+        "content": request.form["content"]
+    }
+    Comment.update_comment(data)
+    return redirect(f"/sighting/{sighting_id}")
+
+
+@app.route("/comment/delete/<int:comment_id>/<int:sighting_id>")
+def delete_comment(comment_id, sighting_id):
+    data = {
+        "id": comment_id
+    }
+    Comment.delete_comment(data)
+    return redirect(f"/sighting/{sighting_id}")
--- flask_app/controllers/sightings.py
+++ flask_app/controllers/sightings.py
@@ -1,3 +1,115 @@
 from flask_app import app
 from flask import render_template, redirect, request, session, flash
 from flask_app.models.sighting import Sighting 
+
+#everyones post
+@app.route("/sightings")
+def sightings():
+    pass
+
+
+#add_sighing_form
+@app.route("/sighting/add")
+def add_sighting_page():
+    if "user_id" not in session:
+        flash("You must be logged in to view this page")
+        return redirect("/")
+    return render_template("sighting_create.html")
+
+
+#add_sighting to db
+@app.route("/sighting/create", methods=["POST"])
+def create_sighting():
+    #1. Validate; request.form its a function flask
+    if not Sighting.validate_sighting(request.form):
+        #2. Always redirect to the route that you were at last!!
+        return redirect("/sighting/add")
+    #Why we need this? data prevents SQL injection, the process of using the data in the url to execute commands in the database
+    data = {
+        "location": request.form["location"],
+        "date": request.form["date"],
+        "time": request.form["time"],
+        "description": request.form["description"],
+        "Intensity": request.form["Intensity"],
+        "num_of_activities": request.form["num_of_activities"],
+        "reaction": data["reaction"],
+    }
+    #call the function that does the query, pass in data to edit th database
+    #call the sighting class and the add_sighting function
+    Sighting.add_sighting(data)
+    return redirect("/sightings")
+
+
+#edit_sighting_form
+@app.route("/sighting/edit/<int:id>")
+def edit_sighting_page(id):
+    if "user_id" not in session:
+        flash("You must be logged in to view this page")
+        return redirect("/")
+    #passing in id; so we need to create a data dictionary
+    data = {
+        "id": id
+    }
+    this_sighting = Sighting.get_one_sighting_by_user(data)
+    return render_template("sighting_edit.html", this_sighting)
+
+
+#edit_sighting_db
+@app.route("/sighting/update", methods=["POST"])
+def update_sighting_page(id):
+        #1. Validate; request.form its a function flask
+    if not Sighting.validate_sighting(request.form):
+        #2. Always redirect to the route that you were at last!!
+        return redirect("/sighting/edit/{id}")
+    #Why we need this? data prevents SQL injection, the process of using the data in the url to execute commands in the database
+    data = {
+        "location": request.form["location"],
+        "date": request.form["date"],
+        "time": request.form["time"],
+        "description": request.form["description"],
+        "Intensity": request.form["Intensity"],
+        "num_of_activities": request.form["num_of_activities"],
+        "reaction": data["reaction"],
+    }
+    #call the function that does the query, pass in data to edit th database
+    #call the sighting class and the add_sighting function
+    Sighting.add_sighting(data)
+    return redirect("/sightings")
+
+
+#view sightings, Route that will show an individual game - visible route 
+@app.route("/sightings/<int:id>")
+def view_sighting(id):
+    #1 Check to see if someone is logged in 
+    if "user_id" not in session:
+        flash("You must be logged in to view this page")
+        return redirect("/")
+    data = {
+        "id": id,
+    }
+    this_sighting = Sighting.get_one_sighting_by_user(data)
+    return render_template("sighting_view.html", this_sighting)
+
+
+#Route that will delete a game, Need the id of the game to delete 
+@app.route("/sighting/delete/<int:id>")
+def delete_sighting(id):
+    #1 check to see if someone is logged in 
+    if "user_id" not in session:
+        return redirect("/")
+    data = {
+        "id": id, # ID of the game
+    }
+    Sighting.delete_sighting(data)
+    return redirect("/")
+
+
+
+# #users dashboard
+# @app.route("/user")
+
+# #view everyones post on dashboard
+# @app.route("/dashboard")
+
+
+#sql_injection
